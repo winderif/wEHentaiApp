e487c7f8a0f1baabbfd964f14b09701d051013a8
winderif <winderif@gmail.com>
2015-07-05 14:43:07 +0800

    Use DaoMaster to create and drop database

--- src/com/example/ehentaiapp/database/EhentaiDBHelper.java
+++ src/com/example/ehentaiapp/database/EhentaiDBHelper.java
@@ -5,19 +5,30 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.example.winderif.ehentaiapp.DaoMaster;
+
 public class EhentaiDBHelper extends SQLiteOpenHelper {
-	// 資料庫名稱
+    private static EhentaiDBHelper instance = null;
+
     public static final String DATABASE_NAME = "ehentai_data.db";
-    // 資料庫版本，資料結構改變的時候要更改這個數字，通常是加一
-    public static final int VERSION = 3;    
-    // 資料庫物件，固定的欄位變數
+    public static final int VERSION = 1;
     private static SQLiteDatabase mDatabase;
-	
+
+    public EhentaiDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
 	public EhentaiDBHelper(Context context, String name, CursorFactory factory,
             int version) {
-		// TODO Auto-generated constructor stub
         super(context, name, factory, version);
 	}
+
+    public static EhentaiDBHelper getInstance(Context context) {
+        if(instance == null) {
+            instance = new EhentaiDBHelper(context.getApplicationContext());
+        }
+        return instance;
+    }
 
     public static SQLiteDatabase getDatabase(Context context) {
         if (mDatabase == null || !mDatabase.isOpen()) {
@@ -30,15 +41,12 @@ public class EhentaiDBHelper extends SQLiteOpenHelper {
  
     @Override
     public void onCreate(SQLiteDatabase db) {
-    	db.execSQL(ItemDAO.CREATE_TABLE);
-    	db.execSQL(TagDAO.CREATE_TABLE);
+        DaoMaster.createAllTables(db, false);
     }
  
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-    	db.execSQL("DROP TABLE IF EXISTS " + ItemDAO.TABLE_NAME);
-    	db.execSQL("DROP TABLE IF EXISTS " + TagDAO.TABLE_NAME);
-    	
+        DaoMaster.dropAllTables(db, true);
         onCreate(db);
     }
 }
