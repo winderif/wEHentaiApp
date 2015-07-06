ce36cbfc1922a5854975f4d88f5ba87616c6859e
winderif <winderif@gmail.com>
2015-07-06 21:36:00 +0800

    add a method to get category text, and a map

--- src/com/example/ehentaiapp/Filter.java
+++ src/com/example/ehentaiapp/Filter.java
@@ -1,5 +1,8 @@
 package com.example.ehentaiapp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Filter {
 	public static final String[] ARRAY_OF_SEARCH_OPTION_FILTER = {
 		"doujinshi", "manga", "artistcg", "gamecg", "western",
@@ -8,6 +11,14 @@ public class Filter {
 	public static final String[] KEY_OF_FILTER = {
 		"f_doujinshi", "f_manga", "f_artistcg", "f_gamecg", "f_western",
 		"f_non-h", "f_imageset", "f_cosplay", "f_asianporn", "f_misc"};
+
+	public static final Map<String, Integer> MAP_FILTER;
+	static {
+		MAP_FILTER = new HashMap<String, Integer>();
+		for(int i=0; i<ARRAY_OF_SEARCH_OPTION_FILTER.length; i++) {
+			MAP_FILTER.put(ARRAY_OF_SEARCH_OPTION_FILTER[i], i);
+		}
+	}
 	
 	public static final String KEY_OF_SEARCH = "f_search";
 	public static final String KEY_OF_APPLY = "f_apply";
@@ -24,6 +35,10 @@ public class Filter {
 	
 	public static String getOption(boolean opt) {
 		return opt ? "1" : "0";
+	}
+
+	public static String getOptionText(int index) {
+		return ARRAY_OF_SEARCH_OPTION_FILTER[index];
 	}
 	
 	public String getOption(int index) {
