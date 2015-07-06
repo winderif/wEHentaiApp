237c460acd64d7b93f0a2753e7c8a73de954542c
winderif <winderif@gmail.com>
2015-07-06 22:00:03 +0800

    use greenDao to access db, use dataloader to get gallery

--- src/com/example/ehentaiapp/HomePageActivity.java
+++ src/com/example/ehentaiapp/HomePageActivity.java
@@ -2,6 +2,7 @@ package com.example.ehentaiapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
@@ -17,20 +18,26 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.ehentaiapp.database.EhentaiDBHelper;
 import com.example.ehentaiapp.database.Item;
 import com.example.ehentaiapp.database.ItemDAO;
-import com.example.ehentaiapp.database.TagDAO;
+import com.example.ehentaiapp.util.DataLoader;
+import com.example.winderif.ehentaiapp.DaoMaster;
+import com.example.winderif.ehentaiapp.DaoSession;
+import com.example.winderif.ehentaiapp.Gallery;
+import com.example.winderif.ehentaiapp.GalleryDao;
+import com.example.winderif.ehentaiapp.GallerysToTags;
+import com.example.winderif.ehentaiapp.GallerysToTagsDao;
+import com.example.winderif.ehentaiapp.TagDao;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 import com.wefika.flowlayout.FlowLayout;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -64,13 +71,25 @@ public class HomePageActivity extends Activity {
 	private ImageButton mImageButton;
 
 	private ItemDAO mItemDAO;
-	private TagDAO mTagDAO;
 	private Item mItem;
 	private ArrayList<String> mTags;
+
+	private GalleryDao galleryDao;
+	private TagDao tagDao;
+	private GallerysToTagsDao gallerysToTagsDao;
 	
 	private String urlOfComic;
 	private boolean isExist = false;
-	
+
+	private Gallery mGallery;
+
+	private DataLoader dataLoader;
+
+	private ParseCoverTask task = null;
+
+	private static final Pattern gUrlPattern =
+			Pattern.compile("http://g\\.e-hentai\\.org/g/(\\d+)/(\\w+)");
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -83,9 +102,7 @@ public class HomePageActivity extends Activity {
 		mTags = new ArrayList<String>();
 		mItemDAO = new ItemDAO(getApplicationContext());
 		mItemDAO.open();
-		mTagDAO = new TagDAO(getApplicationContext());
-		mTagDAO.open();
-		
+
 		setExist(mItemDAO.isExist(urlOfComic));
 		if(isExist()) {			
 			mItem = mItemDAO.get(urlOfComic);
@@ -93,10 +110,29 @@ public class HomePageActivity extends Activity {
 		else {
 			mItem = new Item();
 		}
-		
+
+		dataLoader = DataLoader.getInstance(this);
+
+		setDatabase();
+
+		long gId = 0;
+		Matcher matcher = gUrlPattern.matcher(urlOfComic);
+		if(matcher.find()) {
+			gId = Long.parseLong(matcher.group(1));
+		}
+
 		initView();
-		
-		new ParseCoverTask().execute();
+
+		mGallery = galleryDao.load(gId);
+		if(mGallery == null) {
+			task = new ParseCoverTask();
+			task.execute();
+		}
+		else {
+			showGallery();
+		}
+
+//		new ParseCoverTask().execute();
 	}
 	
 	private void initView() {
@@ -118,11 +154,20 @@ public class HomePageActivity extends Activity {
 		mImageButton.setSelected(mItem.isFavorite());
 	}
 
+	private void setDatabase() {
+//		EhentaiDBHelper helper = EhentaiDBHelper.getInstance(context);
+		SQLiteDatabase db = EhentaiDBHelper.getInstance(this).getDatabase(this);
+		DaoMaster daoMaster = new DaoMaster(db);
+		DaoSession daoSession = daoMaster.newSession();
+		galleryDao = daoSession.getGalleryDao();
+		tagDao = daoSession.getTagDao();
+		gallerysToTagsDao = daoSession.getGallerysToTagsDao();
+	}
+
 	@OnClick(R.id.home_but_1)
 	public void onReadClick(View v) {
 		Intent mIntent = new Intent();				
 		mIntent.setClass(HomePageActivity.this, ScrollActivity.class);
-//		mIntent.setClass(HomePageActivity.this, CominPageActivity.class);
 		mIntent.putExtra("url_comic", urlOfComic);
 		mIntent.putExtra("num_comic_page", mItem.getNumOfPages());
 		startActivity(mIntent);
@@ -147,7 +192,10 @@ public class HomePageActivity extends Activity {
 	private class ParseCoverTask extends AsyncTask<Void, Void, Void> {
 		@Override
 		protected Void doInBackground(Void... param) {
-			// TODO Auto-generated method stub
+
+			mGallery = dataLoader.getGallery(urlOfComic);
+
+			/*
 			try {
 				
 				// 處理沒有漫畫
@@ -176,35 +224,63 @@ public class HomePageActivity extends Activity {
 				}
 			} catch(IOException e) {
 				e.printStackTrace();
-			}		
+			}
+					*/
 			return null;
 		}
 		
 		@Override
 		protected void onPostExecute(Void result) {
+			task = null;
+
+			if(mGallery == null) {
+				mButton.setVisibility(View.INVISIBLE);
+				mImageButton.setVisibility(View.INVISIBLE);
+				return ;
+			}
+			/*
 			if(mItem.getNumOfPages() == 0) {
 				mButton.setVisibility(View.INVISIBLE);
 				mImageButton.setVisibility(View.INVISIBLE);
 			}
-			
-			setComicInfo();
-			
-			imageLoader.displayImage(mItem.getUrlOfComicCover(), mImageView, 
+			*/
+//			setComicInfo();
+			setGalleryInfo();
+
+//			imageLoader.displayImage(mItem.getUrlOfComicCover(), mImageView,
+			imageLoader.displayImage(mGallery.getThumbnail(), mImageView,
 					options, new SimpleImageLoadingListener() {
 				@Override
 		        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
 					mImageView.setImageBitmap(loadedImage);				
 	        	}
 			});
-			
-			
+
+			setGalleryTag();
+
+			/*
 			if(mTags.size() == 0) {
 				mTagLayout.setVisibility(View.GONE);
 			}
 			else {
 				setTagView();
 			}
+			*/
+
 		}	
+	}
+
+	private void showGallery() {
+		imageLoader.displayImage(mGallery.getThumbnail(), mImageView,
+				options, new SimpleImageLoadingListener() {
+					@Override
+					public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+						mImageView.setImageBitmap(loadedImage);
+					}
+				});
+
+		setGalleryInfo();
+		setGalleryTag();
 	}
 	
 	private void setNotFoundComic() {
@@ -222,6 +298,13 @@ public class HomePageActivity extends Activity {
 		sizeTextView.setText("Length:\t" + mItem.getNumOfPages() + " Pages");
 		dateTextView.setText("Date:\t" + mItem.getDatetime());
 		categoryTextView.setText("Category:\t" + mItem.getCategory());
+	}
+
+	private void setGalleryInfo() {
+		titleTextView.setText(mGallery.getTitle());
+		sizeTextView.setText("Length:\t" + mGallery.getSize() + " Pages");
+		dateTextView.setText("Date:\t" + mGallery.getCreated());
+		categoryTextView.setText("Category:\t" + mGallery.getCategoryText());
 	}
 	
 	private void setTagView() {
@@ -245,6 +328,37 @@ public class HomePageActivity extends Activity {
 	            	finish();  	
 				}
 			});
+			mTagLayout.addView(b, layoutParams);
+		}
+	}
+
+	private void setGalleryTag() {
+		List<GallerysToTags> relationTags = mGallery.getTags();
+
+		LayoutParams layoutParams = new FlowLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
+				LayoutParams.WRAP_CONTENT);
+
+		for(GallerysToTags rTag : relationTags) {
+			TextView b = new TextView(HomePageActivity.this);
+			Drawable d = getResources().getDrawable(R.drawable.tag_button);
+			b.setBackgroundDrawable(d);
+			b.setText(rTag.getTag().getName());
+			b.setTextSize(18);
+			b.setGravity(Gravity.CENTER_HORIZONTAL);
+			b.setClickable(true);
+			b.setOnClickListener(new View.OnClickListener(){
+				@Override
+				public void onClick(View v) {
+					showToast(((TextView)v).getText().toString());
+					/*
+					Intent result = getIntent();
+					result.putExtra("tag", ((TextView)v).getText());
+					setResult(1, result);
+					finish();
+					*/
+				}
+			});
+
 			mTagLayout.addView(b, layoutParams);
 		}
 	}
