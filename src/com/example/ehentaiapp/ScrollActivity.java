0795fc11cb3b3070d4ab4f80986b1659e182c947
winderif <winderif@gmail.com>
2015-07-13 13:59:11 +0800

    ScrollView set adapter with Images, rewrite old asynctask , use dataloader to get Image list

--- src/com/example/ehentaiapp/ScrollActivity.java
+++ src/com/example/ehentaiapp/ScrollActivity.java
@@ -1,70 +1,69 @@
 package com.example.ehentaiapp;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import com.example.ehentaiapp.ComicScrollView.ScrollViewListener;
-import com.example.ehentaiapp.ComicScrollView;
-import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiskCache;
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
-import com.nostra13.universalimageloader.core.assist.FailReason;
-import com.nostra13.universalimageloader.core.assist.FailReason.FailType;
-import com.nostra13.universalimageloader.core.assist.ImageScaleType;
-
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.ImageView;
-import android.widget.Toast;
+
+import com.example.ehentaiapp.ComicScrollView.ScrollViewListener;
+import com.example.ehentaiapp.database.EhentaiDBHelper;
+import com.example.ehentaiapp.util.DataLoader;
+import com.example.winderif.ehentaiapp.DaoMaster;
+import com.example.winderif.ehentaiapp.DaoSession;
+import com.example.winderif.ehentaiapp.Gallery;
+import com.example.winderif.ehentaiapp.GalleryDao;
+import com.example.winderif.ehentaiapp.Image;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ScrollActivity extends Activity implements ScrollViewListener {
-	private static final int NUM_OF_SHOW_PAGE = 40;
-	private int numOfComicPage = 0;
+
 	private int idxOfPage = 0;
-	
-	private DisplayImageOptions options;
-	private ImageLoader imageLoader = ImageLoader.getInstance();
-	private ProgressDialog mDialog;
-	
-	private String urlOfComic;
-	private ArrayList<String> urlOfComicPage;
-	private ArrayList<String> urlOfComicPageImg;
-	
-	private LinearLayout layout;
+
 	private ComicScrollView mScrollView;
-	
+	private ImageAdapter adapter;
+	private ProgressDialog mDialog;
+
+	private Gallery mGallery;
+	private List<Image> images;
+	private GalleryDao galleryDao;
+
+	private DataLoader dataLoader;
+
+	private ParserTask task = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_scroll);
+
+		images = new ArrayList<Image>();
 		mScrollView = (ComicScrollView)findViewById(R.id.comic_scrollview);
-		mScrollView.setScrollViewListener(this);
-		layout = (LinearLayout)findViewById(R.id.comic_scroll_layout);
-			
+		adapter = new ImageAdapter(this, images);
+		mScrollView.setAdapter(adapter);
+
+		setDatabase();
+
 		Intent mIntent = super.getIntent();
-		urlOfComic = mIntent.getStringExtra("url_comic");
-		numOfComicPage = mIntent.getIntExtra("num_comic_page", 0); 
-		urlOfComicPageImg = new ArrayList<String>();
-		urlOfComicPage = new ArrayList<String>();
-		
-		new ParserTask().execute();	
+		long id = mIntent.getLongExtra("galleryId", 0);
+
+		mGallery = galleryDao.load(id);
+
+		if(mGallery == null) {
+			return ;
+		}
+		else {
+			dataLoader = DataLoader.getInstance(this);
+
+			task = new ParserTask();
+			task.execute();
+		}
 	}
 	
 	@Override
@@ -78,7 +77,15 @@ public class ScrollActivity extends Activity implements ScrollViewListener {
         dismissProgressDialog();
         super.onDestroy();
     }
-	
+
+	private void setDatabase() {
+		EhentaiDBHelper helper = EhentaiDBHelper.getInstance(getApplicationContext());
+		SQLiteDatabase db = helper.getDatabase();
+		DaoMaster daoMaster = new DaoMaster(db);
+		DaoSession daoSession = daoMaster.newSession();
+		galleryDao = daoSession.getGalleryDao();
+	}
+
 	private void showProgressDialog() {
         if (mDialog == null) {
             mDialog = new ProgressDialog(ScrollActivity.this);
@@ -89,24 +96,12 @@ public class ScrollActivity extends Activity implements ScrollViewListener {
         mDialog.show();
     }
 	
-	private boolean isEndPage() {
-		if(idxOfPage == numOfComicPage) {
-			return false;
-		}
-		else 
-			return true;
-	}
-
     private void dismissProgressDialog() {
         if (mDialog != null && mDialog.isShowing()) {
             mDialog.dismiss();
         }
     }
     
-    private void showToast(String msg) {
-    	Toast.makeText(ScrollActivity.this, msg, Toast.LENGTH_SHORT).show();
-    }
-	
 	private class ParserTask extends AsyncTask<Void, Void, Void> {
 		@Override
 		protected void onPreExecute() {
@@ -115,173 +110,36 @@ public class ScrollActivity extends Activity implements ScrollViewListener {
 		
 		@Override
 		protected Void doInBackground(Void... param) {
-			// TODO Auto-generated method stub
-			
-			try {
-				int numOfUrlPage = numOfComicPage / NUM_OF_SHOW_PAGE
-						+ ((numOfComicPage % NUM_OF_SHOW_PAGE == 0)?0:1);
-				for(int i=0; i<numOfUrlPage; i++) {
-					Document mBookDoc = Jsoup.connect(urlOfComic + "?p=" + i).get();
-					Element mBookTable = mBookDoc.getElementById("gdt");
-					Elements mBookPage = mBookTable.getElementsByClass("gdtm");
-					
-					// Get url of each comic page
-					for(Element page : mBookPage) {
-						urlOfComicPage.add(page.select("a").attr("abs:href"));
-					}
-					
-					// Get image url of the first comic page 
-					Document pageImg = Jsoup.connect(urlOfComicPage.get(0)).get();
-					Element pageImgLink = pageImg.getElementById("i3");
-					urlOfComicPageImg.add(pageImgLink.select("img").attr("abs:src"));
-				}
-			} catch(IOException e) {
-				e.printStackTrace();
-			}		
+
+			images.addAll(dataLoader.getImageList(mGallery));
+
 			return null;
 		}
 		
 		@Override
 		protected void onPostExecute(Void result) {
 			dismissProgressDialog();
-			
-			ImageView image = new ImageView(ScrollActivity.this);
-			image.setId(R.id.scroll_image);
-			imageLoader.displayImage(urlOfComicPageImg.get(0), 
-					image, options, new SimpleImageLoadingListener() {
-				@Override
-		        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-					ImageView imageView = (ImageView) view.findViewById(R.id.scroll_image);
-					int width = ScrollActivity.this.getResources().getDisplayMetrics().widthPixels;
-					imageView.getLayoutParams().width = width;
-					imageView.getLayoutParams().height = loadedImage.getHeight()*width/loadedImage.getWidth();
-					imageView.setImageBitmap(loadedImage);
-					mScrollView.LoadingComplete();
-	        	}
-			});
-			layout.addView(image);
-		}	
-	}
-	
-	@Override
-	public void onBackPressed() {
-		imageLoader.stop();
-		ScrollActivity.this.setResult(RESULT_OK);
-		ScrollActivity.this.finish();   
-		super.onBackPressed();
-	}
-	
-	private class PageLoaderTask extends AsyncTask<Integer, Void, Void> {
-		
-		@Override
-		protected Void doInBackground(Integer... param) {
-			// TODO Auto-generated method stub
-			try {
-				Document pageImg = Jsoup.connect(urlOfComicPage.get(param[0])).get();
-				Element pageImgLink = pageImg.getElementById("i3");
-				urlOfComicPageImg.add(pageImgLink.select("img").attr("abs:src"));
-			} catch(IOException e) {
-				e.printStackTrace();
-			}		
-			return null;
+
+			adapter.notifyDataSetChanged();
 		}
-		
-		@Override
-		protected void onPostExecute(Void result) {			
-			ImageView image = new ImageView(ScrollActivity.this);
-			image.setId(R.id.scroll_image);
-			imageLoader.displayImage(urlOfComicPageImg.get(urlOfComicPageImg.size()-1), 
-					image, options, new SimpleImageLoadingListener() {
-				@Override
-		        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-					ImageView imageView = (ImageView) view.findViewById(R.id.scroll_image);
-					int width = ScrollActivity.this.getResources().getDisplayMetrics().widthPixels;
-					imageView.getLayoutParams().width = width;
-					imageView.getLayoutParams().height = loadedImage.getHeight()*width/loadedImage.getWidth();
-					imageView.setImageBitmap(loadedImage);		
-					mScrollView.LoadingComplete();
-	        	}
-				
-				@Override
-			    public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
-					switch(failReason.getType()) {
-						case IO_ERROR:
-							new PageReLoaderTask().execute(idxOfPage);
-							break;
-						case OUT_OF_MEMORY:
-							imageLoader.clearMemoryCache();
-							new PageReLoaderTask().execute(idxOfPage);
-							break;
-						default:
-							break;
-					}
-			    }
-			});
-			layout.addView(image);
-		}	
 	}
 
-private class PageReLoaderTask extends AsyncTask<Integer, Void, Void> {
-		
-		@Override
-		protected Void doInBackground(Integer... param) {
-			// TODO Auto-generated method stub
-			try {
-				Document pageImg = Jsoup.connect(urlOfComicPage.get(param[0])).get();
-				Element pageImgLink = pageImg.getElementById("i3");
-				urlOfComicPageImg.set(param[0], pageImgLink.select("img").attr("abs:src"));
-			} catch(IOException e) {
-				e.printStackTrace();
-			}		
-			return null;
-		}
-		
-		@Override
-		protected void onPostExecute(Void result) {			
-			ImageView image = new ImageView(ScrollActivity.this);
-			image.setId(R.id.scroll_image);
-			imageLoader.displayImage(urlOfComicPageImg.get(urlOfComicPageImg.size()-1), 
-					image, options, new SimpleImageLoadingListener() {
-				@Override
-		        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-					ImageView imageView = (ImageView) view.findViewById(R.id.scroll_image);
-					int width = ScrollActivity.this.getResources().getDisplayMetrics().widthPixels;
-					imageView.getLayoutParams().width = width;
-					imageView.getLayoutParams().height = loadedImage.getHeight()*width/loadedImage.getWidth();
-					imageView.setImageBitmap(loadedImage);				
-	        	}
-				
-				@Override
-			    public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
-					switch(failReason.getType()) {
-						case IO_ERROR:
-							new PageReLoaderTask().execute(idxOfPage);
-							break;
-						case OUT_OF_MEMORY:
-							imageLoader.clearMemoryCache();
-							new PageReLoaderTask().execute(idxOfPage);
-							break;
-						default:
-							break;
-					}
-			    }
-			});
-			layout.addView(image);
-		}	
+	@Override
+	public void onBackPressed() {
+		super.onBackPressed();
+
+		setResult(RESULT_OK);
+		finish();
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.scroll, menu);
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
@@ -293,14 +151,6 @@ private class PageReLoaderTask extends AsyncTask<Integer, Void, Void> {
 	public void onLoading(ComicScrollView scrollView) {
 		// TODO Auto-generated method stub
 		idxOfPage++;
-		Log.i("PAGE", idxOfPage+" "+numOfComicPage);
-		if(isEndPage()) {
-			new PageLoaderTask().execute(idxOfPage);
-		}
-		else {
-			/**
-			 * End Page
-			 */
-		}
+		Log.i("PAGE", "page: " + idxOfPage);
 	}
 }
